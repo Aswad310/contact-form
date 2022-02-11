e960a4b4b920f7e62cda9dedb4891e7b6eb57ffe
Aswad Ali <71272839+Aswad310@users.noreply.github.com>
2022-02-11 15:34:41 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/public/contactform.html');
+    res.sendFile(__dirname + '../public/contactform.html');
 });
 
 app.post('/', (req, res)=>{
